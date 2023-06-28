c54fdeb2b35fb7751332bbecf6499db20b020888
jovasdevs <jovasdevs@gmail.com>
2023-06-28 18:59:53 -0300

    first commit

---
+++ string_1.py
@@ -0,0 +1,19 @@
+nome = "estudandoPython"
+
+print(nome.upper())
+print(nome.lower())
+print(nome.title())
+
+texto = " estudando Python   "
+
+print(texto + ".")
+print(texto.strip() + ".")
+print(texto.rstrip() + ".")
+print(texto.lstrip() + ".")
+
+menu = "python"
+
+print("@@@ " + menu + " @@@")
+print(menu.center(10))
+print(menu.center(10, "*"))
+print(".".join(menu))
